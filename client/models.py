9b5ca77443c94d2e6a21508c1a1395053e963e29
Fredrik Carlsen <fredrik@fncit.no>
2011-07-17 20:27:36 +0200

    lagt inn enkel retry..

--- client/models.py
+++ client/models.py
@@ -178,10 +178,16 @@ class FTPStorage:
                 if percent >= self.file_upload_percent+2:
                     self.schedule.machine.log_info("uploaded " + str(int(percent)) + "% " + "("+str(uploaded_size) +" MB)" + "of file: " + str(file_name) + ". spent time: " + str(round(time_spent_seconds,2)) + " seconds. speed: " + str(round(float(uploaded_size/(time_spent_seconds/1024)),1)) + " kb/s")
                     self.file_upload_percent = int(percent)
-                    
             except Exception, e:
                 self.schedule.machine.log_error(str(e))
 
+                try:
+                    self.schedule.machine.log_info("Retry upload..")
+                    handle_upload_progress(block)
+                except Exception, e:
+                    self.schedule.machine.log_error(str(e))
+
+                    
         self.schedule.machine.log_info("Start upload folder %s (%s MB) to %s" % (local_file_path, str(round(float(self.file_upload_total_size)/1024/1024, 1)), storage_folder))
         self.connection.cwd("~/")
         f = open(local_file_path, "rb")
