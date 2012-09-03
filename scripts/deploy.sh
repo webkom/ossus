903b4d5f0a0e7ad6e76774bd9fd45b190fe8356a
Fredrik Nygård Carlsen <fredrik@fncit.no>
2012-09-03 03:21:25 +0200

    fix deploy-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -e # Exit on error
 
-
-ssh focus@kontor.focussecurity.no'
+ssh focus@'kontor.focussecurity.no' '
     ssh 'focus'@'10.0.6.31' "
         cd /var/webapps/focus24/focusbackup
         git pull origin master
