2b35c4a2e339d85209e8f418db2ab887754fa0fc
Fredrik Nygård Carlsen <me@frecar.no>
2012-12-26 19:44:30 +0100

    lagre day_folder_path for backup-entries

--- project/app/backup/models.py
+++ project/app/backup/models.py
@@ -2,7 +2,6 @@
 from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.timezone import utc
 
 class Company(models.Model):
     name = models.CharField(max_length=150)
