9fcacba214ea1e1014f5b553defcb0a03c916b1c
Fredrik Nygård Carlsen <me@frecar.no>
2013-01-10 11:08:05 +0100

    fix deploy-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ssh focus@'kontor.focussecurity.no' '
-    ssh 'focus'@'10.0.6.31' "
-        cd /var/webapps/focus24/focusbackup
-        git pull origin master
-        bin/django syncdb
-        bin/django migrate --merge
-        sudo /etc/init.d/httpd restart
-    "
+    cd /var/webapps/focus24/focusbackup
+    git pull origin master
+    bin/django syncdb
+    bin/django migrate --merge
+    sudo /etc/init.d/httpd restart
 '
