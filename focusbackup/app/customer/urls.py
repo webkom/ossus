36d0755e7b824f23d67bbd654f0c52148016c5a7
Fredrik Nygård Carlsen <me@frecar.no>
2014-02-27 17:38:44 +0100

    create links for adding new customers and storages

--- focusbackup/app/customer/urls.py
+++ focusbackup/app/customer/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('focusbackup.app.customer.views',
                        url(r'^$', 'overview', name="customer_overview"),
-                       url(r'^new/$', 'new'),
+                       url(r'^new/$', 'new', name="customer.new"),
                        url(r'^(?P<id>\w+)/view/$', 'view'),
                        url(r'^(?P<id>\w+)/edit/$', 'edit'),
-                       )
+                    )
--- focusbackup/app/storage/urls.py
+++ focusbackup/app/storage/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('focusbackup.app.storage.views',
     url(r'^$', 'overview', name="storage_overview"),
-    (r'^new/$', 'new'),
+    url(r'^new/$', 'new', name="storages.new" ),
     (r'^(?P<id>\w+)/view/$', 'edit'),
     (r'^(?P<id>\w+)/edit/$', 'edit'),
 )
