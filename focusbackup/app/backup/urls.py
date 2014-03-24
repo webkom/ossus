2e256a8ade9160f685a9e18cc1415905dc4a379a
Fredrik Nygård Carlsen <me@frecar.no>
2014-03-24 01:10:06 +0100

    fix url

--- focusbackup/app/backup/urls.py
+++ focusbackup/app/backup/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^new/(?P<machine_id>\w+)/$', 'focusbackup.app.backup.views.new', name="backups.new"),
                        url(r'^(?P<machine_id>\w+)/(?P<id>\w+)/edit/$', 'focusbackup.app.backup.views.edit', name='backups.edit'),
-                       url(r'^(?P<machine_id>\w+)//settings/$', 'focusbackup.api.views.machines.get_settings_for_machine'),
+                       url(r'^(?P<machine_id>\w+)/settings/$', 'focusbackup.api.views.machines.get_settings_for_machine'),
 
                        )
