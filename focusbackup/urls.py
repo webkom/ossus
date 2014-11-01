cda5d0038c3a7e12f6586754117419a3bbb3c9ad
Rolf Erik Lekang <me@rolflekang.com>
2014-11-02 00:50:26 +0100

    Add support for django-nopassword

--- focusbackup/urls.py
+++ focusbackup/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -8,18 +8,22 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    (r'^$', 'focusbackup.app.machine.views.overview'),
-    (r'^accounts/', include('focusbackup.app.accounts.urls')),
-    (r'^machines/', include('focusbackup.app.machine.urls')),
-    (r'^backups/', include('focusbackup.app.backup.urls')),
-    (r'^storages/', include('focusbackup.app.storage.urls')),
-    (r'^customers/', include('focusbackup.app.customer.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^api/', include('focusbackup.api.urls')),
+    url(r'^$', 'focusbackup.app.machine.views.overview'),
+    url(r'^machines/', include('focusbackup.app.machine.urls')),
+    url(r'^backups/', include('focusbackup.app.backup.urls')),
+    url(r'^storages/', include('focusbackup.app.storage.urls')),
+    url(r'^customers/', include('focusbackup.app.customer.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('focusbackup.api.urls')),
 
-    (r'^download_current_agent/', 'focusbackup.core.views.download_current_agent'),
-    (r'^download_current_updater/', 'focusbackup.core.views.download_current_updater'),
-    (r'^download_current_installer/', 'focusbackup.core.views.download_current_installer'),
+    url(r'^download_current_agent/', 'focusbackup.core.views.download_current_agent'),
+    url(r'^download_current_updater/', 'focusbackup.core.views.download_current_updater'),
+    url(r'^download_current_installer/', 'focusbackup.core.views.download_current_installer'),
 )
+
+if 'nopassword' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('', url(r'^accounts/', include('nopassword.urls')))
+else:
+    urlpatterns += patterns('', url(r'^accounts/', include('focusbackup.app.accounts.urls')))
 
 urlpatterns += staticfiles_urlpatterns()
